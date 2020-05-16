ec3745b5f2d115f0b98f64c90e9f778cd2b23c84
newtonsart <newtonsart@github.com>
2020-05-16 14:29:07 +0200

    modified:   asmd.py

--- asmd.py
+++ asmd.py
@@ -3,7 +3,7 @@ from os import system as shell
 from sys import argv
 raw_filename = argv[1]
 filename = argv[1].replace(".asm", "")
-shell(f"nasm -f elf32 -o {filename}.o {raw_filename} ")
-shell(f"ld -m elf_i386 -o {filename} {filename}.o")
+shell(f"nasm -f elf64 -o {filename}.o {raw_filename} ")
+shell(f"ld -m elf_x86_64 -o {filename} {filename}.o")
 shell(f"./{filename}")
-shell(f"rm -r {filename}.o {filename}")
+shell(f"rm -r {filename}.o")
